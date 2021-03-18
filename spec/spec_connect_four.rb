4dc3f0f8325b5e96a001abe8e872109d142125b6
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2021-03-18 22:08:16 +0100

    Added tests for player class

--- spec/spec_connect_four.rb
+++ spec/spec_connect_four.rb
@@ -1,1 +1,13 @@
-require "../script"
+require "../lib/player.rb"
+
+describe Player do 
+    describe "#colorized_disc" do 
+        context "when given colour red" do
+            subject(:player) { described_class.new("player1", "red") } 
+            it "returns red disc" do 
+                player_colour = player.disc_colour
+                expect(subject.colorized_disc(player_colour)).to eq("\e[31m\u26ab\e[0m")
+            end 
+        end     
+    end 
+end 
